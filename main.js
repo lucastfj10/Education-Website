7eb83ee362a2e568b6c18e3fc50831c8dcf2d546
Lucas Daniel <lucastfj10@gmail.com>
2023-06-08 13:01:37 -0300

    8-6-2023

--- main.js
+++ main.js
@@ -5,4 +5,17 @@ window.addEventListener ('scroll', () =>{
 })
 
 // SHOW/HIDE FAQ ANSWER
-const faqs = document
+const faqs = document.querySelectorAll('.faq')
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open')
+
+        // CHANGE ICON
+        const icon = faq.querySelector('.faq_icon i')
+        if (icon.className === 'uil uil-plus') {
+            icon.className = "uil uil-minus"
+        } else {
+            icon.className = "uil uil-plus"
+        }
+    })
+})
