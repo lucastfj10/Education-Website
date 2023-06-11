c3221cbba58e2fcb67b51c36939e95a0e4dcdf3d
Lucas Daniel <lucastfj10@gmail.com>
2023-06-10 22:22:47 -0300

    10-6-2023

--- main.js
+++ main.js
@@ -19,3 +19,23 @@ faqs.forEach(faq => {
         }
     })
 })
+
+// SHOW/HIDE NAV MENU 
+const menu = document.querySelector('.nav_menu')
+const menuBtn = document.querySelector('#open-menu-btn')
+const closeBtn = document.querySelector('#close-menu-btn')
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'flex'
+    closeBtn.style.display = 'inline-block'
+    menuBtn.style.display = 'none'
+})
+
+// CLOSE NAV MENU
+const closeNav = () => {
+    menu.style.display = 'none'
+    closeBtn.style.display = 'none'
+    menuBtn.style.display = 'inline-block'
+}
+
+closeBtn.addEventListener('click', closeNav)
